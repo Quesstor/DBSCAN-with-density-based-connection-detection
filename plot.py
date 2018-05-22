7c225b238fcf6d4c4fc46dcab12e8e942d45fdbe
Janis Held <janisheld@gmail.com>
2018-05-22 22:24:02 +0200

    version update

--- plot.py
+++ plot.py
@@ -6,11 +6,8 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import numpy as np
 import winsound
-from functools import partial
-import os
-
 import pool
-
+from functools import partial
 
 currentid = str(time.time())[-5:]
 plotcounter = 0
@@ -20,21 +17,19 @@ plt.subplots_adjust(left=0.035, right=0.99, top=0.95, bottom=0.03)
 fig = plt.figure(figsize=(60, 36))
 
 
-def clusterColor(i, norm, permutation, autumn=False):
+def clusterColor(i, norm, autumn=False):
     if i == -2: return (1, 0, 0, 1)  # Waypoints
     if i == -1: return (0, 0, 0, .1)  # Outlier
     if i == 0: return (0, 0, 0, 1)  
-    if autumn: return cm.autumn(norm(permutation[i]))
-    return cm.rainbow(norm(permutation[i]))
+    if autumn: return cm.autumn(norm(i))
+    return cm.rainbow(norm(i))
 
 
-def plot(points, labels, writeIndex=False, title="", legend=False, eps=0, text=[], showAxis=True, autumn=False, randomcolor=False, dotSize=20):
+def plot(points, labels, writeIndex=False, title="", legend=False, eps=0, text=[], showAxis=True, autumn=False):
     global currentid, fig, plotcounter
     ax = fig.add_subplot(111)
     dataid = currentid+"."+str(plotcounter)
     plotcounter += 1
-    try: os.stat("plots/")
-    except: os.mkdir("plots/") 
     np.save("plots/"+dataid+"_points", points)
     np.save("plots/"+dataid+"_labels", labels)
 
@@ -45,15 +40,12 @@ def plot(points, labels, writeIndex=False, title="", legend=False, eps=0, text=[
         print("PLOT failed: len(points) == 0")
     points = np.array(points)
     labels = np.array(labels)
-    labelsCount = round(max(labels)*1.1 + 1)
-    norm = Normalize(vmin=0, vmax=labelsCount)
-    permutation = range(int(labelsCount)+1)
-    if randomcolor: permutation = np.random.permutation(int(labelsCount)+1)
+    norm = Normalize(vmin=0, vmax=round(max(labels)*1.1))
 
-    getColorFnc = partial(clusterColor, permutation=permutation, norm=norm, autumn=autumn)
+    getColorFnc = partial(clusterColor, norm=norm, autumn=autumn)
     colors = pool.getThreadPool().map(getColorFnc, labels)
     xs, ys = zip(*points)
-    ax.scatter(xs, ys, color=colors, s=[dotSize]*len(xs))
+    ax.scatter(xs, ys, color=colors, s=[2]*len(xs))
     if eps > 0:
         ax = plt.gca()
         for p in points:
